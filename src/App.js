86ce01ea58ec3a4498bbba15fc0eeca1a9038930
psychoSherlock <pschosherlock@gmail.com>
2024-03-19 23:31:11 +0530

    Setup redux and user state dispatcher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Landing from "./pages/landing/landing";
 import TopBar from "./nav/topBar";
 import "./App.css";
+import Nav from "./nav/navButtons";
 
 function App() {
+  const user = useSelector((state) => state.user);
+
   return (
     <Router>
       <TopBar />
@@ -12,6 +16,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         {/* <Route path="/" element={<TodaysMenu />} /> */}
       </Routes>
+      {user && <Nav />}
     </Router>
   );
 }
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -5,12 +5,17 @@ import landinglogo from "./landing-logo.png";
 import { FaGoogle } from "react-icons/fa";
 import supabase from "../../supabase";
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Landing() {
-  const [user, setUser] = useState(null);
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch(); // dispatch is used to dispatch an action to the redux store
   async function userStatus() {
     const user = await supabase.auth.getUser();
-    setUser(user.data.user); // Set the user state to user.data.user
+    // setUser(user.data.user); // Set the user state to user.data.user
+    dispatch({ type: "SET_USER", payload: user.data.user });
   }
   async function signIn() {
     await supabase.auth.signInWithOAuth({
@@ -19,7 +24,7 @@ function Landing() {
   }
   async function signOut() {
     await supabase.auth.signOut();
-    setUser(null);
+    dispatch({ type: "SET_USER", payload: null });
   }
 
   useEffect(() => {
